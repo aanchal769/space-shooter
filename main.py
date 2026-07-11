243962e227f975c4cd32a7f8e97a1f0fa3d1b134
Aanchal Gupta <aanchalgupta769@gmail.com>
2026-07-11 19:26:06 +0530

    Added bullet system with shooting mechanics

--- main.py
+++ main.py
@@ -10,14 +10,32 @@ player_width=100
 player_height=100
 
 
+
+
+bullet_width=15
+bullet_height=40
 screen=pygame.display.set_mode((screen_width,screen_height))
 player=pygame.image.load("assets/images/player.png")
 player=pygame.transform.scale(player,(player_width,player_height))
+
+
 pygame.display.set_caption("Space-Shooter")
+
+
+bullet=pygame.image.load("assets/images/bullet.png")
+bullet=pygame.transform.scale(bullet,(bullet_width,bullet_height))
+
+
+
 
 player_x=700
 player_y=650
 player_speed=20
+bullet_speed=15
+bullet_x=player_x+ (player_width//2)-(bullet_width//2)
+bullet_y=player_y+(player_height//2)-(bullet_height//2)
+
+bullet_state="ready"
 clock=pygame.time.Clock()
 
 running=True
@@ -29,6 +47,22 @@ while running:
         if event.type==pygame.QUIT:
             running=False
 
+        
+        if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_SPACE:
+                        if(bullet_state=="ready"):
+                                bullet_state="fire"
+                                bullet_x=player_x+ (player_width//2)-(bullet_width//2)
+                                bullet_y=player_y+(player_height//2)-(bullet_height//2)
+
+    if bullet_state=="fire":
+                bullet_y-=bullet_speed
+
+    if bullet_y<0:
+            bullet_state="ready"
+            bullet_y=bullet_y
+            bullet_x=bullet_x
+        
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_RIGHT]:
@@ -54,9 +88,15 @@ while running:
     if player_y > screen_height-player_height:
                 player_y=screen_height-player_height
     
+
+    
     
     screen.fill((10,25,60))
     screen.blit(player,(player_x,player_y))
+    screen.blit(bullet,(bullet_x,bullet_y))
+
+    
+    
     clock.tick(30)
     pygame.display.update()
 
